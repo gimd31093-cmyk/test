2d070eaf0701f8e79284d3676363e20ac44ca9e0
gimd31093-cmyk <gimd31093@gmail.com>
2025-10-18 23:29:33 +0900

    Add files via upload

---
+++ public/resources/js/script.js
@@ -0,0 +1,60 @@
+const dd = console.log;
+
+const Quiz = {
+    json: [],
+    data: [],
+    checks: [],
+
+    async init() {
+        Quiz.json = await $.get(`/api/data.json?d=${new Date().getTime()}`);
+
+        Quiz.hook();
+        Quiz.setData();
+    },
+
+    hook() {
+        $(document)
+            .on("click", ".quiz_btn", Quiz.start)
+            .on("click", ".btn", Quiz.submit)
+    },
+
+    submit(e) {
+        const idx = $(e.target).parents("item").data("idx");
+        const data = Quiz.data.find(v => v.idx == idx);
+        const text = $(`#quiz-${idx}`).val();
+
+        if (data.correct === text) {
+            $(`.btn_box-${idx}`).html(`<div class="btn">정답!</div>`);
+        } else {
+            if (data?.include) {
+                if (data.every(v => text.include(v))) $(`.btn_box-${idx}`).html(`<div class="btn">정답!</div>`);
+                return;
+            }
+
+            $(`.btn_box-${idx}`).html(`<div class="btn">오답..</div>`);
+        }
+    },
+
+    start() {
+        $(`.middle`).html(Quiz.data.map(v => {
+            return `
+                <div class="item item-${v.idx}" data-idx="${v.idx}">
+                    <h2 class="title">${(Quiz.now + 1).padStart(2, "0")}. ${nowData.slove}</h2>
+
+                    <input type="text" id="quiz-${v.idx}" placeholder="답을 입력해주세요.">
+
+                    <div class="btn_box btn_box-${v.idx}">
+                        <div class="btn">제출</div>
+                    </div>
+                </div>
+            `;
+        }));
+    },
+
+    setData() {
+        const mixs = Quiz.json.toSorted(() => Math.random() - .5);
+        Quiz.data = mixs;
+    }
+}
+
+$(() => Quiz.init());
